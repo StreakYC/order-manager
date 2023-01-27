6ed9520f8d470fedae8fc15ad0602156d2261976
Chris Cowan <agentme49@gmail.com>
2023-01-26 19:35:45 -0800

    update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,35 @@
+// @flow
 module.exports = {
-  "parser": "babel-eslint",
-  "env": {
-    "jest": true,
-    "node": true,
-    "es6": true
+  'parser': '@babel/eslint-parser',
+  'env': {
+    'jest': true,
+    'node': true,
+    'es6': true
   },
-  "extends": ["eslint:recommended"],
-  "parserOptions": {
-    "ecmaFeatures": {
+  'extends': ['eslint:recommended'],
+  'parserOptions': {
+    'ecmaFeatures': {
     },
-    "sourceType": "module"
+    'sourceType': 'module'
   },
-  "plugins": [
-    "flowtype"
+  'plugins': [
+    'flowtype'
   ],
-  "rules": {
-    "flowtype/define-flow-type": 1,
-    "flowtype/require-valid-file-annotation": ["error", "always"],
+  'rules': {
+    'flowtype/define-flow-type': 1,
+    'flowtype/require-valid-file-annotation': ['error', 'always'],
 
-    "indent": ["error", 2],
-    "linebreak-style": ["error", "unix"],
-    "quotes": ["error", "single", "avoid-escape"],
-    "semi": ["error", "always"],
-    "no-var": ["error"],
-    "brace-style": ["error"],
-    "array-bracket-spacing": ["error", "never"],
-    "block-spacing": ["error", "always"],
-    "no-spaced-func": ["error"],
-    "no-whitespace-before-property": ["error"],
-    "space-before-blocks": ["error", "always"],
-    "keyword-spacing": ["error"]
+    'indent': ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single', 'avoid-escape'],
+    'semi': ['error', 'always'],
+    'no-var': ['error'],
+    'brace-style': ['error'],
+    'array-bracket-spacing': ['error', 'never'],
+    'block-spacing': ['error', 'always'],
+    'no-spaced-func': ['error'],
+    'no-whitespace-before-property': ['error'],
+    'space-before-blocks': ['error', 'always'],
+    'keyword-spacing': ['error']
   }
 };
